73e37b04c96f8e9b4526788828a5ebf6070b1349
ilynet-0302 <43601053+ilynet-0302@users.noreply.github.com>
2018-10-15 10:47:11 +0300

    Zada4i 15.10.2018
    asd

---
+++ Zad 3 15.10.2018.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int x,a,b,c,S,Pr;
+    cin>>x;
+    a=x/100;
+    b=x/10%10;
+    c=x%10;
+    S=a+b+c;
+    Pr=a*b*c;
+    cout<<"S= "<<S<<endl;
+    cout<<"Pr= "<<Pr<<endl;
+    return 0;
+}
